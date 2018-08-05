8fa4602dceed2b425c6784173082825ada6edafd
Luis <Luis@DESKTOP-VTC82L9>
2018-08-04 23:56:09 -0500

    2018-08-04

--- CapaDatos/Conexion_BaseDeDatos.cs
+++ CapaDatos/Conexion_BaseDeDatos.cs
@@ -9,5 +9,9 @@ namespace CapaDatos
     public class Conexion_BaseDeDatos
     {
         public static string Cn = Properties.Settings.Default.Cn;
+
+        //PRUEBA DE CONEXION DESDE SERVIDOR ALEX HACIA SERVIDOR LENIS
+        //HOOOOOLAA LENIS COMO ESTAS COMO TE TRATA LA VIDA JAJAJAJA
+
     }
 }
